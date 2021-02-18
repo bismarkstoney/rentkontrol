79295ddbed79a01f81d6685340d5ee860dbae236
bismarkstoney <bismarkstoney@yahoo.com>
2021-02-18 23:34:32 +0800

    setup email

--- my_site/settings.py
+++ my_site/settings.py
@@ -167,6 +167,13 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_UNIQUE_EMAIL = True
 
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS={
